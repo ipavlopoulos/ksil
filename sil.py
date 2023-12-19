dc82dc15f8f152409ad0053d921725f5925b70d9
Ioannis Pavlopoulos <annis@aueb.gr>
2023-12-19 15:57:16 +0200

    Update sil.py

--- sil.py
+++ sil.py
@@ -1,15 +1,20 @@
 from sklearn.metrics import silhouette_samples
 
 def aggregated(X, clustering, strategy='macro'):
-  assert strategy in {'macro', 'micro'}
-  point_sils = pd.DataFrame({'sil': silhouette_samples(X, clustering),
+    """
+    Silhouette aggregated with different strategies
+    """
+    
+    assert strategy in {'macro', 'micro'}
+    
+    point_sils = pd.DataFrame({'sil': silhouette_samples(X, clustering),
                                'label':clustering})
-  if strategy == 'micro':
-    return point_sils.mean()
+    if strategy == 'micro':
+        return point_sils.mean()
 
-  elif strategy == 'macro':
-    return point_sils.groupby('label').sil.apply(np.mean)
+    elif strategy == 'macro':
+        return point_sils.groupby('label').sil.apply(np.mean)
 
-  else:
-    print('Not implemented yet')
-    return None
+    else:
+        print('Not implemented yet')
+        return None
