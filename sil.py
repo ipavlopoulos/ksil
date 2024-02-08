fd5006b6ab65391f469fb805ab7dc5c14ae29582
Ioannis Pavlopoulos <annis@aueb.gr>
2024-02-08 12:41:42 +0200

    Added macro-eval

--- sil.py
+++ sil.py
@@ -24,3 +24,21 @@ def micro(X, clustering):
         
     point_sils = pd.DataFrame({'sil': silhouette_samples(X, clustering), 'label':clustering})
     return point_sils.sil.mean()
+
+def sample_per_clust(X, clustering, frac=0.5):
+    """
+    Per-cluster balanced sampling
+    :return: data points subsampled so that a balanced clustering is achieved 
+    """
+    labels_pd = pd.Series(clustering) # a series of the cluster assignments 
+    support = labels_pd.value_counts() # the sizes per label (type)
+    ssample_size = int(support.max()*frac)
+    ssample_size = min(ssample_size, support.min())
+    indices = [] # for each label sample data points
+    for label in support.index:
+        indices.extend(labels_pd[labels_pd==label].sample(ssample_size).index)
+    return pd.Series(X)[indices].values, labels_pd[indices].values
+
+def macro_eval(X, clustering, measure):
+    X, clustering = sample_per_clust(X, clustering)
+    return measure(X, clustering)
